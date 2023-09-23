be5064d5253f0ba0db9e77ca3fbeb95d4313ea93
dileep <yadavdileep557@gmail.com>
2023-09-23 23:20:13 +0530

    commit: Add filter fuction to filter restraunts as for search

--- Src/component/Body.js
+++ Src/component/Body.js
@@ -1,16 +1,64 @@
+import { useState } from "react";
 import { restaurantList } from "./constant";
 import RestrauntCard from "./RestrauntCard";
+import { useState } from "react";
+
+function filterData(searchText, Restaurant){
+
+ const data=   Restaurant.filter((restaurant)=>
+    restaurant.data.name.includes(searchText)
+    
+    
+    )
+    return data;
+}
+
 const Body= ()=>{
+
+    // <- useState returns variable name and function to change the variable -> // 
+    const [searchText, setsearchText]= useState();
+    const [Restaurant, setRestaurants]= useState(restaurantList);
+    //********* */ <- SearchText is state variable -> ***********************//
+    //************<-  State keeps trsck of variable in react*****************//
+
+    //
     return(
+        <>
+        <div className="search-container">
+            <input type="text"
+              placeholder="Search"
+              className="search-input"
+              value={searchText}
+              onChange={(e)=>{
+                setsearchText(e.target.value)
+              }}
+              ></input>
+
+              <button 
+              
+              className="search-btn"
+              onClick={()=>{
+
+                 const Data= filterData(searchText, Restaurant)
+                 setRestaurants(Data);
+
+               } }
+
+             >Search</button>
+
+
+        </div>
         
         <div  className="restraunt-List">
          {
-            restaurantList.map((restaurant)=>{
+            Restaurant.map((restaurant)=>{
                 return <RestrauntCard {...restaurant.data} key={restaurant.data.id}/>
             })
          }
         
         </div>
+
+        </>
         
         
 
